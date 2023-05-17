998c87b93b1816c4e345757ead59b4da7130825c
KoukeNeko <111033412+KoukeNeko@users.noreply.github.com>
2023-05-17 18:22:21 +0800

    fix tailwindcss config file mistake

--- extension/src/components/FavoritePanel/FavoritePanel.jsx
+++ extension/src/components/FavoritePanel/FavoritePanel.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function FavoritePanel() {
   return (
-    <div>Favorites</div>
+    <div className='bg-gradient-to-r from-purple-500 to-pink-500'>Favorites</div>
   )
 }
