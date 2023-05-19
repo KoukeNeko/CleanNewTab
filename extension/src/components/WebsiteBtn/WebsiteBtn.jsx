97551df63d87e8627d86794a698f4dcad4a86f73
KoukeNeko <111033412+KoukeNeko@users.noreply.github.com>
2023-05-19 10:18:22 +0800

    add api to get ico

--- extension/src/components/WebsiteBtn/WebsiteBtn.jsx
+++ extension/src/components/WebsiteBtn/WebsiteBtn.jsx
@@ -12,7 +12,7 @@ export default function WebsiteBtn({
         <div className='flex flex-col items-center justify-center'>
             <div className='bg-white rounded-lg shadow-lg h-20 w-20 flex items-center justify-center'>
                 {/* icon or the website first word of title */}
-                <img className='rounded-lg h-10 w-10' src={`https://www.google.com/s2/favicons?domain=${websiteUrl}`} alt='website icon'/>
+                <img className='rounded-lg h-10 w-10' src={`https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${websiteUrl}/&size=256`} alt='website icon'/>
                 
                 {/* <div className='text-2xl'>1</div>
                 <div className='text-gray-500 text-sm'>1</div> */}
