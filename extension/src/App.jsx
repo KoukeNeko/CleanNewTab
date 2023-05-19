ecc1cb9de1a0a31058c303a8e895664112a3e454
KoukeNeko <111033412+KoukeNeko@users.noreply.github.com>
2023-05-19 09:47:36 +0800

    add icons on the page

--- extension/src/App.jsx
+++ extension/src/App.jsx
@@ -4,8 +4,11 @@ import FavoritePanel from './components/FavoritePanel/FavoritePanel';
 
 function App() {
   return (
-    <div className="container">
-      <FavoritePanel />
+    <div className="w-screen h-screen bg-gradient-to-r from-purple-500 to-pink-500 text-lg">
+      {/* set max width */}
+      <div className='w-full max-w-[1200px] mx-auto'>
+        <FavoritePanel />
+      </div>
     </div>
   );
 }
