e43a8a2da8cbf89db487b66b33cd5991192a7445
KoukeNeko <111033412+KoukeNeko@users.noreply.github.com>
2023-05-19 09:50:20 +0800

    move a component to a single file

--- extension/src/components/FavoritePanel/FavoritePanel.jsx
+++ extension/src/components/FavoritePanel/FavoritePanel.jsx
@@ -1,51 +1,14 @@
 import React from 'react'
 
+import WebsiteBtn from '../WebsiteBtn/WebsiteBtn'
+
 export default function FavoritePanel() {
   return (
     <div>
       <div className='pt-16 text-white text-left mx-16 text-3xl'>Favorites</div>
       {/* create n * 9 grids for favorites websites buttons */}
       <div className='grid grid-cols-9 gap-4 mx-16 mt-8'>
-        <div className='bg-white rounded-lg shadow-lg h-24 w-24 flex items-center justify-center'>
-          <div className='text-2xl'>1</div>
-          <div className='text-gray-500 text-sm'>1</div>
-        </div>
-        <div className='bg-white rounded-lg shadow-lg h-24 w-24 flex items-center justify-center'>
-          <div className='text-2xl'>1</div>
-          <div className='text-gray-500 text-sm'>1</div>
-        </div>
-        <div className='bg-white rounded-lg shadow-lg h-24 w-24 flex items-center justify-center'>
-          <div className='text-2xl'>1</div>
-          <div className='text-gray-500 text-sm'>1</div>
-        </div>
-        <div className='bg-white rounded-lg shadow-lg h-24 w-24 flex items-center justify-center'>
-          <div className='text-2xl'>1</div>
-          <div className='text-gray-500 text-sm'>1</div>
-        </div>
-        <div className='bg-white rounded-lg shadow-lg h-24 w-24 flex items-center justify-center'>
-          <div className='text-2xl'>1</div>
-          <div className='text-gray-500 text-sm'>1</div>
-        </div>
-        <div className='bg-white rounded-lg shadow-lg h-24 w-24 flex items-center justify-center'>
-          <div className='text-2xl'>1</div>
-          <div className='text-gray-500 text-sm'>1</div>
-        </div>
-        <div className='bg-white rounded-lg shadow-lg h-24 w-24 flex items-center justify-center'>
-          <div className='text-2xl'>1</div>
-          <div className='text-gray-500 text-sm'>1</div>
-        </div>
-        <div className='bg-white rounded-lg shadow-lg h-24 w-24 flex items-center justify-center'>
-          <div className='text-2xl'>1</div>
-          <div className='text-gray-500 text-sm'>1</div>
-        </div>
-        <div className='bg-white rounded-lg shadow-lg h-24 w-24 flex items-center justify-center'>
-          <div className='text-2xl'>1</div>
-          <div className='text-gray-500 text-sm'>1</div>
-        </div>
-        <div className='bg-white rounded-lg shadow-lg h-24 w-24 flex items-center justify-center'>
-          <div className='text-2xl'>1</div>
-          <div className='text-gray-500 text-sm'>1</div>
-        </div>
+        <WebsiteBtn />
       </div>
     </div>
 
--- extension/src/components/WebsiteBtn/WebsiteBtn.jsx
+++ extension/src/components/WebsiteBtn/WebsiteBtn.jsx
@@ -4,6 +4,9 @@ export default function WebsiteBtn() {
 
     const [isHover, setIsHover] = React.useState(false)
     return (
-        <div>WebsiteBtn</div>
+        <div className='bg-white rounded-lg shadow-lg h-24 w-24 flex items-center justify-center'>
+          <div className='text-2xl'>1</div>
+          <div className='text-gray-500 text-sm'>1</div>
+        </div>
     )
 }
