93b8cf2a763a68975c1dae98766a6f4f68863416
KoukeNeko <111033412+KoukeNeko@users.noreply.github.com>
2023-05-19 13:34:23 +0800

    add backdrop-opacity to icons

--- extension/src/components/WebsiteBtn/WebsiteBtn.jsx
+++ extension/src/components/WebsiteBtn/WebsiteBtn.jsx
@@ -10,7 +10,7 @@ export default function WebsiteBtn({
     
     return (
         <div className='flex flex-col items-center justify-center'>
-            <div className='bg-white rounded-lg shadow-lg h-20 w-20 flex items-center justify-center'>
+            <div className='bg-white/30 backdrop-opacity-50 backdrop-blur-3xl rounded-lg shadow-lg h-20 w-20 flex items-center justify-center'>
                 {/* icon or the website first word of title */}
                 <img className='rounded-lg h-10 w-10' src={`https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${websiteUrl}/&size=256`} alt='website icon'/>
                 
