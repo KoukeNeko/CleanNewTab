7b8ebd1f8a4dd211cf5ab269cbda8ee70db09be0
KoukeNeko <111033412+KoukeNeko@users.noreply.github.com>
2023-05-19 10:11:33 +0800

    add icons but low quality

--- extension/src/components/FavoritePanel/FavoritePanel.jsx
+++ extension/src/components/FavoritePanel/FavoritePanel.jsx
@@ -8,7 +8,18 @@ export default function FavoritePanel() {
       <div className='pt-16 text-white text-left mx-16 text-3xl'>Favorites</div>
       {/* create n * 9 grids for favorites websites buttons */}
       <div className='grid grid-cols-9 gap-4 mx-16 mt-8'>
-        <WebsiteBtn />
+        <WebsiteBtn websiteName='test' websiteUrl='https://tailwindcss.com/' />
+        <WebsiteBtn websiteName='Google' websiteUrl='https://google.com/' />
+        <WebsiteBtn websiteName='YouTube' websiteUrl='https://YouTube.com/' />
+        <WebsiteBtn websiteName='GitHub' websiteUrl='https://github.com/' />
+        <WebsiteBtn websiteName='test' websiteUrl='https://tailwindcss.com/' />
+        <WebsiteBtn websiteName='Google' websiteUrl='https://google.com/' />
+        <WebsiteBtn websiteName='YouTube' websiteUrl='https://YouTube.com/' />
+        <WebsiteBtn websiteName='GitHub' websiteUrl='https://github.com/' />
+        <WebsiteBtn websiteName='test' websiteUrl='https://tailwindcss.com/' />
+        <WebsiteBtn websiteName='Google' websiteUrl='https://google.com/' />
+        <WebsiteBtn websiteName='YouTube' websiteUrl='https://YouTube.com/' />
+        <WebsiteBtn websiteName='GitHub' websiteUrl='https://github.com/' />
       </div>
     </div>
 
--- extension/src/components/WebsiteBtn/WebsiteBtn.jsx
+++ extension/src/components/WebsiteBtn/WebsiteBtn.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 
-export default function WebsiteBtn() {
+export default function WebsiteBtn({
+    websiteName,
+    websiteUrl,
+}) {
 
     const [isHover, setIsHover] = React.useState(false)
+    console.log(websiteUrl);
+    
     return (
-        <div className='bg-white rounded-lg shadow-lg h-24 w-24 flex items-center justify-center'>
-          <div className='text-2xl'>1</div>
-          <div className='text-gray-500 text-sm'>1</div>
+        <div className='flex flex-col items-center justify-center'>
+            <div className='bg-white rounded-lg shadow-lg h-20 w-20 flex items-center justify-center'>
+                {/* icon or the website first word of title */}
+                <img className='rounded-lg h-10 w-10' src={`https://www.google.com/s2/favicons?domain=${websiteUrl}`} alt='website icon'/>
+                
+                {/* <div className='text-2xl'>1</div>
+                <div className='text-gray-500 text-sm'>1</div> */}
+            </div>
+            <div className='text-white text-sm mt-2'>{websiteName}</div>
         </div>
     )
 }
