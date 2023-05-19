d9bbbe4f4384793044da4009aeab76c2679ed455
KoukeNeko <111033412+KoukeNeko@users.noreply.github.com>
2023-05-19 14:43:07 +0800

    fit the icon bg size to Safari

--- extension/src/components/FavoritePanel/FavoritePanel.jsx
+++ extension/src/components/FavoritePanel/FavoritePanel.jsx
@@ -7,10 +7,15 @@ export default function FavoritePanel() {
     <div>
       <div className='pt-16 text-white text-left mx-16 text-3xl'>Favorites</div>
       {/* create n * 9 grids for favorites websites buttons */}
-      <div className='grid grid-cols-9 gap-4 mx-16 mt-8 '>
+      <div className='grid grid-cols-12 gap-2 mx-16 mt-8 '>
         <WebsiteBtn websiteName='test' websiteUrl='https://tailwindcss.com/' />
         <WebsiteBtn websiteName='Google' websiteUrl='https://google.com/' />
         <WebsiteBtn websiteName='YouTube' websiteUrl='https://YouTube.com/' />
+        <WebsiteBtn websiteName='GitHub' websiteUrl='https://github.com/' />
+        <WebsiteBtn websiteName='test' websiteUrl='https://tailwindcss.com/' />
+        <WebsiteBtn websiteName='Google' websiteUrl='https://google.com/' />
+        <WebsiteBtn websiteName='YouTube' websiteUrl='https://YouTube.com/' />
+        
         <WebsiteBtn websiteName='GitHub' websiteUrl='https://github.com/' />
         <WebsiteBtn websiteName='test' websiteUrl='https://tailwindcss.com/' />
         <WebsiteBtn websiteName='Google' websiteUrl='https://google.com/' />
@@ -20,6 +25,14 @@ export default function FavoritePanel() {
         <WebsiteBtn websiteName='Google' websiteUrl='https://google.com/' />
         <WebsiteBtn websiteName='YouTube' websiteUrl='https://YouTube.com/' />
         <WebsiteBtn websiteName='GitHub' websiteUrl='https://github.com/' />
+        <WebsiteBtn websiteName='GitHub' websiteUrl='https://github.com/' />
+        <WebsiteBtn websiteName='test' websiteUrl='https://tailwindcss.com/' />
+        <WebsiteBtn websiteName='Google' websiteUrl='https://google.com/' />
+        <WebsiteBtn websiteName='YouTube' websiteUrl='https://YouTube.com/' />
+        <WebsiteBtn websiteName='GitHub' websiteUrl='https://github.com/' />
+        <WebsiteBtn websiteName='test' websiteUrl='https://tailwindcss.com/' />
+        <WebsiteBtn websiteName='Google' websiteUrl='https://google.com/' />
+        <WebsiteBtn websiteName='YouTube' websiteUrl='https://YouTube.com/' />
       </div>
     </div>
 
--- extension/src/components/WebsiteBtn/WebsiteBtn.jsx
+++ extension/src/components/WebsiteBtn/WebsiteBtn.jsx
@@ -61,7 +61,7 @@ export default function WebsiteBtn({
 
     return (
         <div onContextMenu={handleContextMenu} className='flex flex-col items-center justify-center'>
-            <div className='bg-white/30 backdrop-opacity-50 backdrop-blur-3xl rounded-lg shadow-lg h-20 w-20 flex items-center justify-center'>
+            <div className='bg-white/30 backdrop-opacity-50 backdrop-blur-3xl rounded-lg shadow-lg h-16 w-16 flex items-center justify-center'>
                 {/* icon or the website first word of title */}
                 <img
                     className='rounded-lg h-10 w-10'
